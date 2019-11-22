41ed62932b828fd988135f62f6861c17ff2741b5
bagy2017 <solt2309@stthomas.edu>
2019-11-22 13:10:51 -0800

    Added post request handler.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const express = require('express');
+var bodyParser = require('body-parser')
 
 // Constants
 const PORT = 8080;
@@ -8,8 +9,18 @@ const HOST = '0.0.0.0';
 
 // App
 const app = express();
+app.use(bodyParser.text({type: '*/*'}));
+
 app.get('/', (req, res) => {
-  res.send('Service X is alive');
+  res.send('Service X is alive\n');
+});
+
+app.post("/1", (req, res) => {
+  res.send(req.body);
+});
+
+app.post("/2", (req, res) => {
+  res.send(req.body);
 });
 
 app.listen(PORT, HOST);
